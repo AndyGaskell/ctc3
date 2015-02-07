bc23fb4717d7a6e49d6514fbeedfbdb50845f144
Andy Gaskell <andygaskelluk@yahoo.co.uk>
2015-02-07 14:39:00 +0000

    ok-wip

--- index.php
+++ index.php
@@ -27,10 +27,24 @@ $json_data = file_get_contents ( "http://tinfoil.bodaegl.com/api/all" );
 
 $data_array = json_decode( $json_data );
     
-
+$data_array = $data_array->requests;
 
 $dump = print_r( $data_array, TRUE );
+/*
 
+    [requests] => Array
+        (
+            [0] => stdClass Object
+                (
+                    [body] => 1
+                    [body_req_id] => FOI-14-0206
+                    [id] => 1
+                    [title] => Scottish Country Dancing
+                    [type] => FOI
+                )
+
+
+*/
 
 ?>
 
@@ -98,7 +112,27 @@ $dump = print_r( $data_array, TRUE );
           
           
           <?php
-          echo $dump;
+          #echo $dump;
+          
+          
+          
+/*
+
+    [requests] => Array
+        (
+            [0] => stdClass Object
+                (
+                    [body] => 1
+                    [body_req_id] => FOI-14-0206
+                    [id] => 1
+                    [title] => Scottish Country Dancing
+                    [type] => FOI
+                )
+
+
+*/          
+          
+          
           ?>
 
           <h2 class="sub-header">Section title</h2>
@@ -106,126 +140,27 @@ $dump = print_r( $data_array, TRUE );
             <table class="table table-striped">
               <thead>
                 <tr>
-                  <th>#</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
+                  <th>body</th>
+                  <th>body_req_id</th>
+                  <th>id</th>
+                  <th>title</th>
+                  <th>type</th>
                 </tr>
               </thead>
               <tbody>
+              <?php
+              foreach ($data_array as $data_item) {
+                ?>
                 <tr>
-                  <td>1,001</td>
-                  <td>Lorem</td>
-                  <td>ipsum</td>
-                  <td>dolor</td>
-                  <td>sit</td>
-                </tr>
-                <tr>
-                  <td>1,002</td>
-                  <td>amet</td>
-                  <td>consectetur</td>
-                  <td>adipiscing</td>
-                  <td>elit</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>Integer</td>
-                  <td>nec</td>
-                  <td>odio</td>
-                  <td>Praesent</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>libero</td>
-                  <td>Sed</td>
-                  <td>cursus</td>
-                  <td>ante</td>
-                </tr>
-                <tr>
-                  <td>1,004</td>
-                  <td>dapibus</td>
-                  <td>diam</td>
-                  <td>Sed</td>
-                  <td>nisi</td>
-                </tr>
-                <tr>
-                  <td>1,005</td>
-                  <td>Nulla</td>
-                  <td>quis</td>
-                  <td>sem</td>
-                  <td>at</td>
-                </tr>
-                <tr>
-                  <td>1,006</td>
-                  <td>nibh</td>
-                  <td>elementum</td>
-                  <td>imperdiet</td>
-                  <td>Duis</td>
-                </tr>
-                <tr>
-                  <td>1,007</td>
-                  <td>sagittis</td>
-                  <td>ipsum</td>
-                  <td>Praesent</td>
-                  <td>mauris</td>
-                </tr>
-                <tr>
-                  <td>1,008</td>
-                  <td>Fusce</td>
-                  <td>nec</td>
-                  <td>tellus</td>
-                  <td>sed</td>
-                </tr>
-                <tr>
-                  <td>1,009</td>
-                  <td>augue</td>
-                  <td>semper</td>
-                  <td>porta</td>
-                  <td>Mauris</td>
-                </tr>
-                <tr>
-                  <td>1,010</td>
-                  <td>massa</td>
-                  <td>Vestibulum</td>
-                  <td>lacinia</td>
-                  <td>arcu</td>
-                </tr>
-                <tr>
-                  <td>1,011</td>
-                  <td>eget</td>
-                  <td>nulla</td>
-                  <td>Class</td>
-                  <td>aptent</td>
-                </tr>
-                <tr>
-                  <td>1,012</td>
-                  <td>taciti</td>
-                  <td>sociosqu</td>
-                  <td>ad</td>
-                  <td>litora</td>
-                </tr>
-                <tr>
-                  <td>1,013</td>
-                  <td>torquent</td>
-                  <td>per</td>
-                  <td>conubia</td>
-                  <td>nostra</td>
-                </tr>
-                <tr>
-                  <td>1,014</td>
-                  <td>per</td>
-                  <td>inceptos</td>
-                  <td>himenaeos</td>
-                  <td>Curabitur</td>
-                </tr>
-                <tr>
-                  <td>1,015</td>
-                  <td>sodales</td>
-                  <td>ligula</td>
-                  <td>in</td>
-                  <td>libero</td>
-                </tr>
+                  <td><?php echo $data_item->body; ?></td>
+                  <td><?php echo $data_item->body_req_id ?></td>
+                  <td><?php echo $data_item->id ?></td>
+                  <td><?php echo $data_item->title ?></td>
+                  <td><?php echo $data_item->type ?></td>    
+                </tr>                         
+                <?php
+              }
+              ?>
               </tbody>
             </table>
           </div>
